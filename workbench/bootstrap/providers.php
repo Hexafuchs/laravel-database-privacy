3f5ae85dbad21ecd2502efbed8916bbd75ae5ea4
Leo <leo@hexafuchs.de>
2024-06-30 16:19:07 +0200

    :technologist: Add workbench config

---
+++ workbench/bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    \Hexafuchs\PrivacyFriendlyDatabaseSessionHandler\PrivacyFriendlyDatabaseSessionHandlerServiceProvider::class,
+    \Hexafuchs\DynamicArtisanServiceProvider\DynamicConsoleSupportServiceProvider::class,
+    \Hexafuchs\DynamicArtisanServiceProvider\DynamicArtisanServiceProvider::class,
+    \Hexafuchs\DynamicArtisanServiceProvider\PackageServiceProvider::class,
+];
